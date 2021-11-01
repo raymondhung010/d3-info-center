1d642005c9c35b2f9643c78cde6c23e14d02790c
raymondhung010 <raymondhung010@gmail.com>
2021-11-02 02:25:32 +0800

    Updates

---
+++ precache-manifest.bd06ae3b5d0a295c3e97da352accfe03.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b3aadcd3385481f4c5f601d826018fb",
+    "url": "/react-admin-dashboard/index.html"
+  },
+  {
+    "revision": "011648c988c8d3fd6cb7",
+    "url": "/react-admin-dashboard/static/css/main.c722d9b5.chunk.css"
+  },
+  {
+    "revision": "03030b5920ea0952178c",
+    "url": "/react-admin-dashboard/static/js/2.8bc4da82.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/react-admin-dashboard/static/js/2.8bc4da82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fc2792f05c68fc32983",
+    "url": "/react-admin-dashboard/static/js/3.4965e8aa.chunk.js"
+  },
+  {
+    "revision": "011648c988c8d3fd6cb7",
+    "url": "/react-admin-dashboard/static/js/main.17fef061.chunk.js"
+  },
+  {
+    "revision": "81741ef511a73513c786",
+    "url": "/react-admin-dashboard/static/js/runtime-main.7e79ebb8.js"
+  }
+]);
